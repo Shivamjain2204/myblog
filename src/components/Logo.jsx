f9cefd92177c4ca596d6ddf9cff8326af4eb74dd
Shivam <jainshivam2204@gmail.com>
2024-01-06 12:27:37 +0530

    Logo Added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import LogoImg from "../assets/logo.jpg"
+ 
 
 function Logo({width = '100px'}) {
   return (
     <div>
-      Logo
+      <img src={LogoImg} style={{width}} alt="" />
     </div>
   )
 }
